fef26fead5f0316f10add8667552808569d31260
dev-rashidul <rashidul.contact@gmail.com>
2023-09-26 00:01:51 +0600

    Home and Facts Page done

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// JavaScript for Carousel
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    items:1
+})

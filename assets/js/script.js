e8ba8d651e2f6be0998c9802d278169499dddecd
dev-rashidul <rashidul.contact@gmail.com>
2023-09-26 16:26:14 +0600

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
-// JavaScript for Carousel
+// JavaScript for Safety Slider
 
-$('.owl-carousel').owlCarousel({
+$('.safety-slider').owlCarousel({
+    loop:true,
+    margin:50,
+    nav:false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    },
+    navText: [' <i class="fa-solid fa-chevron-left"></i>', ' <i class="fa-solid fa-chevron-right"></i>']
+
+})
+// JavaScript for Testimonial Slider
+
+$('.testimonial-slider').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
